3b229976b8efa696f573a8508ebf4e9979746ec4
dookda <sakda.homhuan@gmail.com>
2025-07-26 16:40:37 +0700

    update route from click

--- lab14_routing/src/components/Map.jsx
+++ lab14_routing/src/components/Map.jsx
@@ -7,8 +7,9 @@ import './Map.css';
 export default function Map() {
     const mapContainer = useRef(null);
     const mapRef = useRef(null);
-    const startPoint = useState([]);
-    const endPoint = useState([])
+    const markerRef = useRef(null);
+    const [startPoint, setStartPoint] = useState([99.011715, 18.814504]);
+    const [endPoint, setEndPoint] = useState([98.968707, 18.8013])
 
     useEffect(() => {
         const map = new maplibregl.Map({
@@ -17,6 +18,12 @@ export default function Map() {
             center: [100, 18],
             zoom: 8
         })
+
+        map.on('click', (e) => {
+            console.log(e)
+            setEndPoint([e.lngLat.lng, e.lngLat.lat])
+        })
+
         mapRef.current = map
         return () => map.remove()
     }, []);
@@ -24,24 +31,35 @@ export default function Map() {
     useEffect(() => {
         const map = mapRef.current;
         if (!map) return;
-
-        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/99.011715%2C18.814504%3B98.968707%2C18.8013?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiZG9va2RhIiwiYSI6ImNscTM3azN3OTA4dmEyaXF1bmg3cXRvbDUifQ.d1Ovd_n9PwJqc_MdGS66-A`)
+        const mbToken = 'pk.eyJ1IjoiZG9va2RhIiwiYSI6ImNscTM3azN3OTA4dmEyaXF1bmg3cXRvbDUifQ.d1Ovd_n9PwJqc_MdGS66-A';
+        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${startPoint.toString()};${endPoint.toString()}?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=${mbToken}`)
             .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                const geojson = {
-                    type: 'Feature',
-                    properties: {},
-                    geometry: data.routes[0].geometry
+            .then(({ routes, waypoints }) => {
+                // console.log(data);
+                try {
+                    if (map.getLayer('route') || map.getSource('route')) {
+                        map.removeLayer('route');
+                        map.removeSource('route');
+                        endMarker.remove()
+                    }
+                } catch (error) {
+                    console.log("not found route");
                 }
 
-                const boundsArr = bbox(geojson);
+                const route = {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: routes[0].geometry
+                }
+
+                const boundsArr = bbox(route);
                 map.fitBounds([[boundsArr[0], boundsArr[1]], [boundsArr[2], boundsArr[3]]], { padding: 20 });
 
                 map.addSource('route', {
                     type: 'geojson',
-                    data: geojson
+                    data: route
                 })
+
                 map.addLayer({
                     id: 'route',
                     type: 'line',
@@ -53,14 +71,15 @@ export default function Map() {
                 })
 
                 new maplibregl.Marker({ color: "#880000", })
-                    .setLngLat(data.waypoints[0].location)
+                    .setLngLat(waypoints[0].location)
                     .addTo(map)
 
-                new maplibregl.Marker({ color: "#04782fff", })
-                    .setLngLat(data.waypoints[1].location)
+                if (markerRef.current !== null) markerRef.current.remove()
+                markerRef.current = new maplibregl.Marker({ color: "#04782fff", })
+                    .setLngLat(waypoints[1].location)
                     .addTo(map)
             })
-    }, [])
+    }, [endPoint])
 
     return (
         <div
